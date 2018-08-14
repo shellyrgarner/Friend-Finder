c129fd19aed58101a4fead2d4683cf3352ce2ccc
shellyrgarner <sgrenea@gmail.com>
2018-08-13 21:13:10 -0400

    footers work, api link work, homepage updates

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -4,16 +4,16 @@ var friendsData = [
         "name": "Jolly Test",
         "photo": "https://goo.gl/images/EXddA1",
         "scores": [
-            5,
-            1,
-            4,
-            4,
-            3,
-            3,
-            3,
-            3,
-            4,
-            1
+            "5",
+            "1",
+            "4",
+            "4",
+            "3",
+            "3",
+            "3",
+            "3",
+            "4",
+            "1"
             //31
         ]
     },
@@ -21,16 +21,16 @@ var friendsData = [
         "name": "Judy Blue",
         "photo": "https://goo.gl/images/peLir4",
         "scores": [
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2"
             //20
         ]
     },
@@ -38,16 +38,16 @@ var friendsData = [
         "name": "Pop Hoppers",
         "photo": "https://goo.gl/images/C3oqtY",
         "scores": [
-            1,
-            2,
-            3,
-            3,
-            2,
-            3,
-            2,
-            4,
-            2,
-            5
+            "1",
+            "2",
+            "3",
+            "3",
+            "2",
+            "3",
+            "2",
+            "4",
+            "2",
+            "5"
             //27
         ]
     },
@@ -55,16 +55,16 @@ var friendsData = [
         "name": "Tina Tockers",
         "photo": "https://goo.gl/images/paqY1E",
         "scores": [
-            5,
-            5,
-            1,
-            1,
-            1,
-            2,
-            1,
-            1,
-            1,
-            3
+            "5",
+            "5",
+            "1",
+            "1",
+            "1",
+            "2",
+            "1",
+            "1",
+            "1",
+            "3"
             //21
         ]
     },
@@ -72,16 +72,16 @@ var friendsData = [
         "name": "Rocket",
         "photo": "https://goo.gl/images/6h25bN",
         "scores": [
-            1,
-            3,
-            1,
-            1,
-            1,
-            1,
-            1,
-            2,
-            3,
-            3
+            "1",
+            "3",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "2",
+            "3",
+            "3"
             //17
         ]
     },
--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -1,12 +1,12 @@
 var path = require("path");
 
-var friendsData = require('../data/friends.js');
+var friends = require('../data/friends.js');
 // var surveyData = [];
 
 module.exports = function(app) {
 
     app.get('/api/friends', function(req, res) {
-         res.json("friendData: " + friends);
+         res.json(friends);
     });
 
     app.post('/api/friends', function(req, res) {
